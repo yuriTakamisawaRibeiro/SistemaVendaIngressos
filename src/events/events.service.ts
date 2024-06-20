48d0e603dea5d774b010be019b3024f09904f604
Yuri Takamisawa Ribeiro <110643200+yuriTakamisawaRibeiro@users.noreply.github.com>
2024-06-19 21:22:45 -0300

    add crud events

---
+++ src/events/events.service.ts
@@ -0,0 +1,48 @@
+import { HttpCode, Injectable } from '@nestjs/common';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class EventsService {
+
+  constructor(private prismaService: PrismaService) {
+
+  }
+  
+  create(createEventDto: CreateEventDto) {
+    return this.prismaService.event.create({
+      data: {
+        ...createEventDto,
+        date: new Date(createEventDto.date)
+      }
+    })
+  }
+
+  findAll() {
+    return this.prismaService.event.findMany()
+  }
+
+  findOne(id: string) {
+    return this.prismaService.event.findUnique({
+      where: {id : id}
+    })
+  }
+
+  update(id: string, updateEventDto: UpdateEventDto) {
+    return this.prismaService.event.update({
+      data: {
+        ...updateEventDto,
+        date: new Date(updateEventDto.date),
+      },
+      where: { id },
+    })
+  }
+
+  @HttpCode(200)
+  remove(id: string) {
+    return this.prismaService.event.delete({
+      where: {id},
+    })
+  }
+}
